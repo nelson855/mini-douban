e081192cec21be4e911ad5a959a084dfd935a103
nelson855 <chenran199104@163.com>
2026-05-07 14:57:40 +0800

    fix: update broken movie poster urls

---
+++ backend/src/main/resources/db/migration/V5__fix_movie_poster_urls.sql
@@ -0,0 +1,7 @@
+UPDATE movies
+SET poster_url = 'https://image.tmdb.org/t/p/w500/ySf9I93w6kJiaEb2T4zpoQjY8PR.jpg'
+WHERE title = 'Farewell My Concubine';
+
+UPDATE movies
+SET poster_url = 'https://image.tmdb.org/t/p/w500/z3MXFdFjrYmoxOEFgE1wuphwo2a.jpg'
+WHERE title = 'The Wandering Earth';
